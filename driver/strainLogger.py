9bebd3dd95201f37fef99fff371883589136e634
Ethan Mayer <ethan.mayer@comcast.net>
2022-10-09 19:17:10 -0500

    re-enable pigpio

--- driver/strainLogger.py
+++ driver/strainLogger.py
@@ -2,7 +2,7 @@ import time
 from csv import writer
 from datetime import datetime
 from datetime import timedelta
-#import pigpio
+import pigpio
 
 # Append a list as a row to the CSV
 def append_list_as_row(write_obj, list_of_elem):
@@ -12,9 +12,9 @@ def append_list_as_row(write_obj, list_of_elem):
     csv_writer.writerow(list_of_elem)
 
 # Initialize GPIO pins ############################################################
-# pi = pigpio.pi() # Initialize GPIO on the pi
-# pi.set_pull_up_down(26, pigpio.PUD_DOWN) # Set pull down resistor on pin 26 for reading voltage
-# pi.set_mode(26, pigpio.INPUT) # Set pin 26 to output
+pi = pigpio.pi() # Initialize GPIO on the pi
+pi.set_pull_up_down(26, pigpio.PUD_DOWN) # Set pull down resistor on pin 26 for reading voltage
+pi.set_mode(26, pigpio.INPUT) # Set pin 26 to output
 
 # Initialize CSV file for recording IMU data ########################################
 timestr = time.strftime("%Y%m%d-%H%M%S")
@@ -31,7 +31,7 @@ try:
         while True:
             now = datetime.now()
             delt = now - start
-            csvList = [delt.microseconds/1000.00,"H"]#pi.read(26)]
+            csvList = [delt.microseconds/1000.00,pi.read(26)]
             append_list_as_row(write_obj, csvList)#pi.read(26)) # Read input voltage on pin 26 and write to csv
             time.sleep(0.008) # sleep for 10ms (lowest sample period)
 except KeyboardInterrupt:
